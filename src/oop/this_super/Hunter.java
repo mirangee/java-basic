d397b9580f6639422faf595249a601a73564d39a
mirangee <pmrang@live.co.kr>
2024-02-26 15:29:29 +0900

    Java OOP(접근제한자, 은닉)

--- src/oop/this_super/Hunter.java
+++ src/oop/this_super/Hunter.java
@@ -3,6 +3,11 @@ package oop.this_super;
 public class Hunter extends Player {
     String pet;
 
+    Hunter(String name) {
+        super(name);
+        this.pet = "둥냥이";
+    }
+
     @Override
     void characterInfo() {
         super.characterInfo();
--- src/oop/this_super/MainClass.java
+++ src/oop/this_super/MainClass.java
@@ -8,14 +8,30 @@ public class MainClass {
         p1.characterInfo();
 
         System.out.println("---------------------------");
+
         Player p2 = new Player("플레이어2");
-        System.out.println("Main에서 p2의 주소값: " + p2);
         p2.characterInfo();
 
         System.out.println("---------------------------");
-        Player p3 = new Player("플레이어3");
-        System.out.println("Main에서 p3의 주소값: " + p3
-        );
-        p2.characterInfo();
+
+        Player p3 = new Player("플레이어3", 100);
+        p3.characterInfo();
+
+        System.out.println("---------------------------");
+        p1.attack(p2);
+        p2.attack(p3);
+        p3.attack(p1);
+
+        System.out.println("---------------------------");
+        Warrior w1 = new Warrior("전사짱짱맨");
+        w1.characterInfo();
+
+        System.out.println("---------------------------");
+        Mage m1 = new Mage("비겁한법사");
+        m1.characterInfo();
+
+        System.out.println("---------------------------");
+        Hunter h1 = new Hunter("사냥이");
+        h1.characterInfo();
     }
 }
